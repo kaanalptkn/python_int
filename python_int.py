cc7fd2f07aac5edb6122b49a43f84bc19b5122e5
Kaan Alptakin <92170029+kaanalptkn@users.noreply.github.com>
2023-09-26 13:59:20 +0100

    Added Read me text

---
+++ python_int.py
@@ -0,0 +1,5 @@
+## Task 
+The provided code stub reads two integers from STDIN, _a_ and _b_ . Add code to print three lines where:
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
